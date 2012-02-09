0024912dd05d4712713886412df446c8a5cb9e60
Kenrick Beckett <kenrick.beckett@gmail.com>
2012-02-09 03:00:56 -0500

    Rebuild Board to use overlays, and added background image

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -4,3 +4,240 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 
 
 
+(function( $ ){
+  
+ //SAFE GLOBALS
+ $.ludo = {
+	mainPath : [
+		[7, 1], [8, 1],	[9, 1],
+		[9, 2], [9, 3], [9, 4], [9, 5], [9, 6],
+		[10, 7], [11, 7], [12, 7], [13, 7], [14, 7],
+		[15, 7], [15, 8], [15, 9],
+		[14, 9], [13, 9], [12, 9], [11, 9], [10, 9],
+		[9, 10], [9, 11], [9, 12], [9, 13], [9, 14],
+		[9, 15], [8, 15], [7, 15],
+		[7, 14], [7, 13], [7, 12], [7, 11], [7, 10],
+		[6, 9], [5, 9], [4, 9], [3, 9], [2, 9],
+		[1, 9], [1, 8], [1, 7],
+		[2, 7], [3, 7], [4, 7], [5, 7], [6, 7],
+		[7, 6], [7, 5], [7, 4], [7, 3], [7, 2]
+	],
+	center : [
+		[7, 7], [8, 7], [9, 7],
+		[7, 8], [8, 8], [9, 8],
+		[7, 9], [8, 9], [9, 9]
+	],
+	startPoint: {
+		red	: [7, 14],
+		blue : [14, 9], 
+	},
+	playerAreas: {
+		green	: [1, 1],
+		yellow  : [10, 1], 
+		red 	: [1, 10],
+		blue 	: [10, 10]
+	},		
+	
+ }
+
+
+ //Creates the board
+ var boardMethods = {
+ 	   init : function( options ) { 
+	
+	      	// $(this).board("createGridLinesOverlay");
+			$(this).board("createBoxOverlay");
+			
+			$(this).delegate(".path", "click", function(){
+				var x = parseInt($(this).attr("data-x")),
+				    y = parseInt($(this).attr("data-y"));
+				$.ludo.socket.emit("moveTo", {token: "token3", cord : [ x, y] });
+			});
+			
+			
+	    },
+	    createBoxOverlay : function( ) {
+	       	var self = boardMethods, 
+			line = 1,
+			lineDiv = $("<div />", {
+				id : "line" +  line,
+				class : "line"
+			}),
+			boxOverlay = $("<div />", {
+				id : "boxOverlay",
+				"class" : "boardOverlay",
+			}),
+			count = 0;
+
+			for (var i = 0; i < 225; i++) {
+				
+				   
+				if(count % 15 === 0 && count > 0 ){
+					
+					count = 0;
+					line += 1;
+					
+					boxOverlay.append(lineDiv);
+										
+					lineDiv = $("<div />", {
+						id : "line" +  line,
+						class : "line"
+					});
+				}
+
+				count++;
+				
+				var cord = {
+					x	: count,
+					y	: line
+				};
+
+				var cordinate = cord.x + ", " + cord.y;
+
+				var id = "x" + cord.x + "y" + cord.y;
+				var	box = $("<div />", {
+						'id' : id, 
+						'class' : "box", 
+						'data-x' : cord.x, 
+						'data-y' : cord.y,
+						'line'   : line,
+					});
+					
+
+					lineDiv.append(box);
+					
+				if(line == 15 && count == 15)
+					boxOverlay.append(lineDiv);
+					
+			};
+			boxOverlay = self.markBoxes(boxOverlay);
+			$(this).append(boxOverlay);
+			
+	    },
+	    createGridLinesOverlay : function( ) { 
+			var lines = $("<div />", {
+				id : "gridLinesOverlay",
+				"class" : "boardOverlay"
+			}); 	
+			for (var i = 1; i <= 14; i++) {
+				var horizontalLine = $("<div />",{
+					"class" : "hLine",
+				});
+				
+				var verticalLine =  $("<div />",{
+					"class" : "vLine",
+				});
+				
+				horizontalLine.css("top", (6.667 * i) + "%");
+				verticalLine.css("left", (6.667 * i) + "%");
+				
+				lines.append(horizontalLine);
+				lines.append(verticalLine);
+
+			}
+			
+			$(this).append(lines);
+			
+	    },
+	    markBoxes : function(boxOverlay) { 
+	        var self = boardMethods;
+			//Match Path 
+			for (var i = 0; i < $.ludo.mainPath.length; i++)
+			{
+				var x = $.ludo.mainPath[i][0],
+					y = $.ludo.mainPath[i][1];
+
+				$(boxOverlay).find("#x" + x + "y" + y ).addClass("path");
+	
+			}
+			
+			
+			//Match Center 
+			for (var i = 0; i < $.ludo.center.length; i++)
+			{
+				var x = $.ludo.center[i][0],
+					y = $.ludo.center[i][1];
+
+				$(boxOverlay).find("#x" + x + "y" + y ).addClass("center");
+	
+			}
+			
+			
+			$.each($.ludo.playerAreas, function(player, areaPoint){
+
+				var area = self.determineArea(areaPoint);
+
+				$.each(area, function(index, path){
+
+					var x = path[0],
+						y = path[1];
+
+					$(boxOverlay).find("#x" + x + "y" + y ).addClass(player);
+
+				});
+
+
+			});
+			
+		   return boxOverlay;	
+	    },
+	
+		determineArea : function(startPoint){
+			var area = new Array();
+
+			for (var y = startPoint[1]; y <= startPoint[1] + 5; y++) {
+
+				for (var x = startPoint[0]; x <= startPoint[0] + 5; x++) {
+					area.push([x, y]);
+				};
+
+			};
+
+			return area;
+
+		},
+  };
+
+  $.fn.board = function( method ) {
+
+   	 // Method calling logic
+	    if ( boardMethods[method] ) {
+	      return boardMethods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	    } else if ( typeof method === 'object' || ! method ) {
+	      return boardMethods.init.apply( this, arguments );
+	    } else {
+	      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+	    }    
+
+  };
+
+
+  $.fn.animateTo = function() {
+	
+	    if(arguments.length == 1)
+	    {
+			cord = arguments[0];
+			callback = function(){};
+	    }
+		var left = (cord[0] - 1) * 6.69;
+		var top = (cord[1] - 1) * 6.67;
+
+		$(this).animate({ left : left + "%", top : top + "%"}, 500, function(){
+			callback();
+		});
+
+  };
+
+  $.fn.jumpTo = function( cord ) {
+
+		var left = (cord[0] - 1) * 6.69;
+		var top = (cord[1] - 1) * 6.67;
+
+		$(this).css({ left : left + "%", top : top + "%"})
+  };
+
+
+})( jQuery );
+
+
+
--- static/js/script.js
+++ static/js/script.js
@@ -4,172 +4,39 @@
 
 */
 
-var socket = undefined;
-var animateTo = undefined;
-
 $(function(){
-	socket = io.connect();
 	
-	socket.on("connect", function(){
+	$.ludo.socket = io.connect();
+	
+	$.ludo.socket.on("connect", function(){
 		log("connected to the server");
 		
-		socket.on('moveTo', function (data) {
+		$.ludo.socket.on('moveTo', function (data) {
 	     	var cord = data.cord,
 				token = data.token;
 			
-			animateTo($("#" + token), cord, function(){
-				
+			$("#" + token).animateTo(cord);	
 			
-			});	
-				
-			
+			log(data);
 	    });
 		
 	});
-
-	//Define Paths
 	
-	var mainPath = [
-			[7, 1], [8, 1],	[9, 1],
-			[9, 2], [9, 3], [9, 4], [9, 5], [9, 6],
-			[10, 7], [11, 7], [12, 7], [13, 7], [14, 7],
-			[15, 7], [15, 8], [15, 9],
-			[14, 9], [13, 9], [12, 9], [11, 9], [10, 9],
-			[9, 10], [9, 11], [9, 12], [9, 13], [9, 14],
-			[9, 15], [8, 15], [7, 15],
-			[7, 14], [7, 13], [7, 12], [7, 11], [7, 10],
-			[6, 9], [5, 9], [4, 9], [3, 9], [2, 9],
-			[1, 9], [1, 8], [1, 7],
-			[2, 7], [3, 7], [4, 7], [5, 7], [6, 7],
-			[7, 6], [7, 5], [7, 4], [7, 3], [7, 2]];
-			
-	var center = [
-			[7, 7], [8, 7], [9, 7],
-			[7, 8], [8, 8], [9, 8],
-			[7, 9], [8, 9], [9, 9]
-	];
+	$("#board").board();
 	
-	var startPoint = {
-		red	: [7, 14],
-		blue : [14, 9]
-	};
-	
-	var playerAreas = {
-			green	: [1, 1],
-			yellow  : [10, 1], 
-			red 	: [1, 10],
-			blue 	: [10, 10]
-	};		
-	
-	
-	//Create Board
-	var line = 1,
-		count = 0;
-	for (var i=0; i < 225; i++) {
-		
-		if(count % 15 === 0 && count > 0 ){
-			count = 0;
-			line += 1;
-		}
-		
-		count++;
-		var cord = {
-			x	: count,
-			y	: line
-		};
-		
-		var cordinate = cord.x + ", " + cord.y;
-		
-		var id = cord.x + "-" + cord.y;
-			boxHtml = "<div id='"+ id +"' data-x='"+ cord.x +"' data-y='"+ cord.y +"' class='box'> <span></span> </div>";
-		
-		$("#board").append(boxHtml);
-		
-	};
-	
-	//Label the path with class
-	$.each(mainPath, function(index, path){
-		
-		var x = path[0],
-			y = path[1];
-			
-		$("#" + x + "-" +  y).addClass("path");	
-			
+	var token = $("<div />", {
+		html : "<img class='token-img' src='/img/red.png' />", 
+		class : "token",
+		id : "token3", 
 	});
 	
-	//Label the center
-	$.each(center, function(index, path){
-		
-		var x = path[0],
-		y = path[1];
-			
-		$("#" + x + "-" +  y ).addClass("center");
-		
-	});
+	token.jumpTo($.ludo.startPoint.red);
 	
-	//Determine where the user areas are :
-	function determineArea(startPoint){
-		var area = new Array();
-		
-		for (var y = startPoint[1]; y <= startPoint[1] + 5; y++) {
-			
-			for (var x = startPoint[0]; x <= startPoint[0] + 5; x++) {
-				area.push([x, y]);
-			};
-			
-		};
-		
-		return area;
-		
-	}
-	
-	$.each(playerAreas, function(player, areaPoint){
-		
-		var area = determineArea(areaPoint);
-		
-		$.each(area, function(index, path){
-			
-			var x = path[0],
-				y = path[1];
-
-			$("#" + x + "-" +  y).addClass(player);
-			
-		});
-		
-		
-	});	
-
-	
-	//Animmate around the board
-	animateTo = function(token ,cord, callback){
-
-		//derive the position
-		var left = (cord[0] - 1) * 6.69;
-		var top = (cord[1] - 1) * 6.67;
-
-		token.animate({ left : left + "%", top : top + "%"}, 500, function(){
-			callback();
-		}) 
-	}
+	$("#board").append(token)
 	
 
 
-	function around(loc){
-		var black = $("#black");
-		if(loc != mainPath.length - 1)
-			loc = loc + 1;
-		else
-			loc = 0;
-		
-		
-		var cord = mainPath[loc];
-		animateTo(black, cord, function(){
-			around(loc)
-		});
-
-	}
-
-
+/*
 	$("#7-14").css("background-color", "#da251c");
 	$("#8-14").css("background-color", "#da251c");
 	$("#8-13").css("background-color", "#da251c");	
@@ -205,16 +72,12 @@ $(function(){
 	$("#8-4").css("background-color", "#f8c301");
 	$("#8-5").css("background-color", "#f8c301");
 	$("#8-6").css("background-color", "#f8c301");
+
+*/	
 	
+
 	
-	animateTo($("#token3"), startPoint.red, function(){});
-	
-	$(".box").click(function(){
-		var x = parseInt($(this).attr("data-x")),
-			y = parseInt($(this).attr("data-y"));
-		
-		socket.emit("moveTo", {token: "token3", cord : [ x, y] });
-	});
+
 	
 });	
 
