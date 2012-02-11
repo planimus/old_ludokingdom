e3342e5bbbdaab34f87c6e2860fb79aef2c43248
Kenrick Beckett <kenrick.beckett@gmail.com>
2012-02-11 12:43:57 -0500

    Added gzip to socket.io

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var connect = require('connect')
     , io = require('socket.io')
     , port = (process.env.PORT || 8081);
 
+
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+//io.set('log level', 1);                    // reduce logging
+io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
 //Setup Express
 var server = express.createServer();
 server.configure(function(){
