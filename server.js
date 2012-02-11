1267af5f69f49a4805a74cf6c75fc638054c740a
Kenrick Beckett <kenrick.beckett@gmail.com>
2012-02-11 12:52:09 -0500

    Added gzip to socket.io fixed

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ var connect = require('connect')
     , io = require('socket.io')
     , port = (process.env.PORT || 8081);
 
-
-io.enable('browser client minification');  // send minified client
-io.enable('browser client etag');          // apply etag caching logic based on version number
-io.enable('browser client gzip');          // gzip the file
-//io.set('log level', 1);                    // reduce logging
-io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-    'websocket'
-  , 'flashsocket'
-  , 'htmlfile'
-  , 'xhr-polling'
-  , 'jsonp-polling'
-]);
-
 //Setup Express
 var server = express.createServer();
 server.configure(function(){
@@ -132,6 +119,21 @@ ludo.init();
 
 //Setup Socket.IO
 var io = io.listen(server);
+
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+//io.set('log level', 1);                    // reduce logging
+io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+
+
 io.sockets.on('connection', function(socket){
     
     socket.emit("tokenLocations", {
